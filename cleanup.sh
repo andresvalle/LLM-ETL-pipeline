751a974c5077aad8265ec5259c3903d2b53b2345
andresvalle <andres.e.valle.p@gmail.com>
2024-11-16 18:45:16 -0600

    documented the scripts

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This values must match the ones used for creation
 STACK_NAME="llm-etl"
 REGION="us-east-1"
 
@@ -8,7 +9,9 @@ BUCKET=$(aws cloudformation describe-stacks --region $REGION \
                                    --query "Stacks[].Outputs[?OutputKey=='bucketName'].OutputValue" \
                                    --output text)
 
+# Emptying the bucket
 aws s3 rm s3://$BUCKET --recursive
 
+# Destroying the stack
 aws cloudformation delete-stack --stack-name $STACK_NAME \
                                 --region $REGION
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-
+# Change the defaults as needed
 STACK_NAME="llm-etl"
 REGION="us-east-1"
 
+# Stack creation
 aws cloudformation deploy --template-file ./iac/cfnStack.yaml \
                           --stack-name $STACK_NAME \
                           --capabilities  CAPABILITY_NAMED_IAM \
                           --region $REGION \
                           --tags environment=testing
 
+# Uploading the datasets to S3
 BUCKET=$(aws cloudformation describe-stacks --region $REGION \
                                    --stack-name $STACK_NAME \
                                    --query "Stacks[].Outputs[?OutputKey=='bucketName'].OutputValue" \
                                    --output text)
-
 aws s3 cp ./sample_datasets s3://$BUCKET --recursive
